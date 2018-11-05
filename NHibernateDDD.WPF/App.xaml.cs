af145342b412af7a4ad0fe0336c2bccd44c80c94
Don Woodford <dwoodford@motivaction.com>
2018-11-04 18:20:32 -0700

    Add WPF project

---
+++ NHibernateDDD.WPF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NHibernateDDD.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+           // base.OnStartup(e);
+
+            var window = new MainWindow();
+            var vm = new EmployeeViewModel();
+
+            vm.LoadEmployees();
+
+            window.DataContext = vm;
+            window.Show();
+        }
+    }
+}
---
+++ NHibernateDDD.WPF/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateDDD.WPF
+{
+    public class EmployeeViewModel
+    {
+        public ObservableCollection<Employee> Employees
+        {
+            get;
+            set;
+        }
+
+        public void LoadEmployees()
+        {
+            var employees = new ObservableCollection<Employee>
+            {
+                Employee.Create("Don", "Woodford", new CEO()).Value
+            };
+
+            Employees = employees;
+        }
+    }
+}
